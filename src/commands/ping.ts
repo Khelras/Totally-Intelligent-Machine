0ffba8449e9e91d902f5feeea0e61d1185909a2a
AnJello <anjello.30bohol@gmail.com>
2026-09-01 23:45:12 +1200

    Ping Command

---
+++ src/commands/ping.ts
@@ -0,0 +1,26 @@
+import { Message, SlashCommandBuilder, type ChatInputCommandInteraction } from 'discord.js';
+import type { Command } from '../types/command.js';
+
+// Ping Command
+const ping: Command = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with the bot\'s current latency.'),
+
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+        const { resource } = await interaction.reply({ content: 'Pinging...', withResponse: true  });
+        const sent: Message<boolean> | null | undefined = await resource?.message;
+
+        // Ensure Sent Message is Valid
+        if (!sent) {
+            await interaction.editReply('⚠️ Failed to calculate latency.');
+            return;
+        }
+
+        // Calculate how long it took Discord to receive our reply (Round-trip time).
+        const latency: number = sent.createdTimestamp - interaction.createdTimestamp;
+        await interaction.editReply(`🏓 Pong! \nLatency: ${latency}ms. \nAPI: ${interaction.client.ws.ping}ms.`);
+    },
+};
+
+export default ping;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { client } from './client.js';
 import { config } from './config.js';
+import { loadCommands } from './handlers/commandHandler.js';
 import { loadEvents } from './handlers/eventHandler.js';
 
 // Main Function
 async function main(): Promise<void> {
-    // Load and Run all of the Events
+    // Load and Run all of the Commands and Events
+    await loadCommands(client);
     await loadEvents(client);
 
     // Login to the Client
