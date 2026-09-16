d0fadf23bef27da81aa5be08270e2d77b30d55b0
AnJello <anjello.30bohol@gmail.com>
2026-09-16 15:54:27 +1200

    Initialising the Schema

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import { client } from './client.js';
 import { config } from './config.js';
 import { loadCommands } from './handlers/commandHandler.js';
 import { loadEvents } from './handlers/eventHandler.js';
+import { initializeSchema } from './database/schema.js';
 
 // Main Function
 async function main(): Promise<void> {
+    // Initialise the Schema into the Database
+    initializeSchema();
+
     // Load and Run all of the Commands and Events
     await loadCommands(client);
     await loadEvents(client);
